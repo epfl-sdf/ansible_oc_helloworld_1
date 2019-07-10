ab4b93330723489ab14802327572ff9b52a7452d
Christian Zufferey <christian@zufferey.com>
2019-07-10 14:39:52 +0200

    Refactorisé le script

--- deploy_nginx.sh
+++ deploy_nginx.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
+#petit script pour démarrer le binz
+#190710.1437
+
+echo -e "
+
+Usage:
+
+Create
+./deplay_nginx state=latest depl_name=toto replicas=1
+
+Delete
+./deplay_nginx state=absent depl_name=toto replicas=1
+
+"
+read -p "Appuyer une touche pour démarrer..."
+
 
 ansible-playbook --connection=local -e "$1 $2 $3" apply.yaml
