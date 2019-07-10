5a3ef0761b3d681e54ada263c17be896dbce6389
Christian Zufferey <christian@zufferey.com>
2019-07-10 14:56:48 +0200

    wip

--- deploy_nginx.sh
+++ deploy_nginx.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 #petit script pour démarrer le binz
-#190710.1437
+#190710.1449
 
 echo -e "
 
 Usage:
 
 Create
-./deplay_nginx state=latest depl_name=toto replicas=1
+./deploy_nginx.sh state=latest depl_name=toto
+
+ou
+./deploy_nginx.sh state=latest depl_name=toto replicas=1
 
 Delete
-./deplay_nginx state=absent depl_name=toto replicas=1
+./deploy_nginx.sh state=absent depl_name=toto
 
 "
 read -p "Appuyer une touche pour démarrer..."
 
-
+#echo $0 $1 $2 $3
 ansible-playbook --connection=local -e "$1 $2 $3" apply.yaml
