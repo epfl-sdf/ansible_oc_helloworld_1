d7a27550d96e01132219b507e6880ee6df1e3515
batKem <demahomirdahk@gmail.com>
2019-07-12 14:53:41 +0200

    fixed persistent volume deletion script

--- delete_pvs.sh
+++ delete_pvs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-oc delete pv nfs-pv-bat --as system:admin
+
 oc delete pvc nfs-pvc
+oc delete pv nfs-pv-bat --as system:admin
+
+
--- pv_init.sh
+++ pv_init.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+
 oc apply -f persistent_volume.yaml --as system:admin
 oc apply -f test-pvc.yaml
+
+
+
+
+
+
