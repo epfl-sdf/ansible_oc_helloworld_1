a8bb5cb84d7d32c148f58bf87bd26017c8349a3b
batKem <demahomirdahk@gmail.com>
2019-07-15 12:03:14 +0200

    made parameter names clearer and added nfs_ip param

--- ans.sh
+++ ans.sh
@@ -2,7 +2,7 @@
 
 
 if [[ $1 == '-help' || $1 == '-h' ]]; then
-  printf   'Usage: \n    ./ans.sh  state=[latest/absent/present] appname=name replicas=1 dbip=128.179.146.8 dbname=db1 \n'
+  printf   'Usage: \n    ./ans.sh  state=[latest/absent/present] app_name=opp-app replicas=1 db_ip=128.179.146.8 db_name=db1  nfs_ip=128.179.146.8\n'
 else
   ansible-playbook -t all -e "$1 $2 $3 $4 $5" main.yaml --connection=local
 fi
