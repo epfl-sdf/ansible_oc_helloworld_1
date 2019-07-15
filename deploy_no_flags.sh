ff3555c269026913c8fe865ecf39cc1c092f3673
batKem <demahomirdahk@gmail.com>
2019-07-15 11:27:26 +0200

    added task flags to avoid using the modified openshift module

---
+++ deploy_no_flags.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+if [[ $1 == '-help' || $1 == '-h' ]]; then
+  printf   'Usage: \n    ./ans.sh  state=[latest/absent/present] appname=name replicas=1 dbip=128.179.146.8 dbname=db1 \n'
+else
+  ansible-playbook -t no-flags -e "$1 $2 $3 $4 $5" main.yaml --connection=local
+fi
